0782e364c900449d4bc62e85176f576fd0b690f2
ryo <s21c1021um@s.chibakoudai.jp>
2022-11-03 12:38:01 +0900

    test

--- test.bash
+++ test.bash
@@ -9,17 +9,16 @@ ng () {
 
 res=0
 
-### I/0 TEST ###
 out=$(seq 5 | ./plus)
+
 [ "${out}" = 15 ] || ng ${LINENO}
 
-### STRANGE INPUT ###
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]     ||ng ${LINENO}
-["${out}" = "" ]  ||ng ${LINENO}
+[ "$?" = 1 ]      ||ng ${LINENO}
+[ "${out}" = "" ] ||ng ${LINENO}
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]     ||ng ${LINENO}
+[ "$?" = 1 ]       ||ng ${LINENO}
 [ "${out}" = "" ] ||ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
