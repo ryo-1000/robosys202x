e37d4206663d7bf889031fe5f0f238bda4c6dad4
ryo <s21c1021um@s.chibakoudai.jp>
2022-11-03 12:29:30 +0900

    test

--- test.bash
+++ test.bash
@@ -16,11 +16,11 @@ out=$(seq 5 | ./plus)
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]     ||ng ${LINENO}
-["${out}" = ""]  ||ng ${LINENO}
+["${out}" = "" ]  ||ng ${LINENO}
 
 out=$(echo | ./plus)
 [ "$?" = 1 ]     ||ng ${LINENO}
-[ "${out}" = ""] ||ng ${LINENO}
+[ "${out}" = "" ] ||ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
